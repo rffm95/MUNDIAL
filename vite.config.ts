6afa03b845ff9b07d92cb1028eba26a7fad16f57
rffm95 <sucessomacico@gmail.com>
2026-06-19 15:40:56 +0100

    pronto para o pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/ROULLETA-MUNDIAL/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
